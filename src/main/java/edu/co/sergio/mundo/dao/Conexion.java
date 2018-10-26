17354b148e0c7b814806062318aaaa9e1651b028
Labing <Labing@LabingXEON-PC>
2018-10-26 12:43:02 -0500

    0.1

--- src/main/java/edu/co/sergio/mundo/dao/Conexion.java
+++ src/main/java/edu/co/sergio/mundo/dao/Conexion.java
@@ -1,51 +1,43 @@
 package edu.co.sergio.mundo.dao;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
 public class Conexion {
-	
-	private static Connection CONEXION=null;
-    	public static Connection getConnection() throws URISyntaxException{
-        String HOST = "-----:----"; 
-        String DATABASE = "----";
-        String USER = "----";
-        String PASS = "----";
 
-//      URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//      String username = dbUri.getUserInfo().split(":")[0];
-//      String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" +
-                HOST + "/" + DATABASE +
-                "?user=" + USER  + "&password=" + 
-                PASS + "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-    
-		   if(CONEXION == null){
-			  	try {
-					CONEXION = DriverManager.getConnection(dbUrl);
-                        	} catch (SQLException e) {
-					System.out.println("Connection Failed! Check output console");
-					e.printStackTrace();
-				}
+    private static Connection CONEXION = null;
 
-				
-		   }
-		   return CONEXION;
-	}
-	
-	public static void closeConnection(){
-		 try {
-			 if(CONEXION!=null){
-				 CONEXION.close();
-				 CONEXION=null;
-			 }
-			 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    
-	}
-	
+    public static Connection getConnection() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        if (CONEXION == null) {
+            try {
+                CONEXION = DriverManager.getConnection(dbUrl, username, password);
+            } catch (SQLException e) {
+                System.out.println("Connection Failed! Check output console");
+                e.printStackTrace();
+            }
+
+        }
+        return CONEXION;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (CONEXION != null) {
+                CONEXION.close();
+                CONEXION = null;
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 
 }
